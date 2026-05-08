43ee1c724249ad46cab70eaae6e8cea7f5d3abc0
naoya25 <onaoya2002@gmail.com>
2026-05-08 22:21:26 +0900

    style(test): テストコードのインポート文を整理 インポート文のフォーマットを統一し、可読性を向上させた

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'package:flutter_tmp/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('ホームが表示され、2画面目へ遷移できる', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(child: MyApp()),
-    );
+    await tester.pumpWidget(const ProviderScope(child: MyApp()));
 
     expect(find.text('トップ画面'), findsOneWidget);
 
